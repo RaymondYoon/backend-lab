9f486b1131110add38e6da8546da271848150e7a
Raymond Yoon <yoonjunsang@naver.com>
2025-04-22 18:22:55 +0900

    JWT 인증을 위한 사용자 상세 정보 서비스 클래스 추가

---
+++ src/main/java/com/example/backproject1/domain/jwt/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.backproject1.domain.jwt;
+
+import com.example.backproject1.domain.user.entity.User;
+import com.example.backproject1.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER") // or user.getRoles() if you use roles
+                .build();
+    }
+}
